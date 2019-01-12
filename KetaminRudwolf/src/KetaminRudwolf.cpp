11679331902c15b7131759d37635d036d4f88de2
Vasile2kappa <andy.dorcu@yahoo.com>
2019-01-12 23:51:07 +0200

    Setup project for GLEW and GLFW

--- KetaminRudwolf/src/KetaminRudwolf.cpp
+++ KetaminRudwolf/src/KetaminRudwolf.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <windows.h>
+#define GLEW_STATIC
+#include <GLEW\glew.h>
+#define GLFW_INCLUDE_NONE
+#include <GLFW\glfw3.h>
 
 int main() {
+
+	glewInit();
+	glfwInit();
+
 	std::cout << "Rudwolf, the dog on da' Key!" << std::endl;
 	return 0;
 }
